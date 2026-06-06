f9bff65e11211d461f5f9fa5772cfeb7b8f6c49f
aaryaprasad06 <prasadaarya740@gmail.com>
2026-06-06 09:48:55 +0530

    Implemented resume ranking engine

---
+++ Backend/ranking.py
@@ -0,0 +1,31 @@
+from embedding_engine import generate_embedding
+from similarity import calculate_similarity
+
+
+def rank_resumes(job_description, resumes):
+    ranked_candidates = []
+
+    job_embedding = generate_embedding(job_description)
+
+    for candidate_name, resume_text in resumes.items():
+
+        resume_embedding = generate_embedding(
+            resume_text
+        )
+
+        score = calculate_similarity(
+            resume_embedding,
+            job_embedding
+        )
+
+        ranked_candidates.append({
+            "candidate": candidate_name,
+            "score": round(score*100, 2)
+        })
+
+    ranked_candidates.sort(
+        key=lambda x: x["score"],
+        reverse=True
+    )
+
+    return ranked_candidates
